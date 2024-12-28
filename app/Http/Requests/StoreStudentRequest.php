73d73cf0a20a7c00899a746576eb77b04bf3a4f0
mehidy <mehidy.gb@gmail.com>
2024-12-28 10:42:17 +0600

    update student store req class

--- app/Http/Requests/StoreStudentRequest.php
+++ app/Http/Requests/StoreStudentRequest.php
@@ -14,29 +14,28 @@ class StoreStudentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'title' => 'nullable|string|max:255',
-            'firstName' => 'required|string|max:255',
-            'lastName' => 'required|string|max:255',
-            'email' => 'required|email|max:255|unique:students,student_data->email',
-            'phone' => 'nullable|string|max:20',
-            'dob' => 'nullable|date_format:d-m-Y',
-            'maritualStatus' => 'nullable|string|max:255',
-            'gender' => 'nullable|string|max:50',
-            'nationality' => 'nullable|string|max:255',
-            'countryResidence' => 'nullable|string|max:255',
-            'countryBirth' => 'nullable|string|max:255',
+            'title' => 'required|string|max:255', // Title is required
+            'firstName' => 'required|string|max:255', // First Name is required
+            'lastName' => 'required|string|max:255', // Last Name is required
+            'email' => 'required|email|max:255|unique:students,student_data->email', // Email is required and unique
+            'phone' => 'required|string|max:20', // Phone is required
+            'dob' => 'required|date_format:d-m-Y', // Date of Birth is required
+            'gender' => 'required|string|max:50', // Gender is required
+            'maritualStatus' => 'required|string|max:255', // Marital Status is required
+            'addressLine1' => 'required|string|max:255', // Address Line 1 is required
+            'addressLine2' => 'nullable|string|max:255', // Address Line 2 is nullable
+            'townCity' => 'required|string|max:255', // Town/City is required
+            'state' => 'required|string|max:255', // State is required
+            'postCode' => 'required|string|max:20', // Post Code is required
+            'country' => 'required|string|max:255', // Country is required
+
+            // All other fields remain nullable
             'nativeLanguage' => 'nullable|string|max:255',
             'passportName' => 'nullable|string|max:255',
             'passportIssueLocation' => 'nullable|string|max:255',
             'passportNumber' => 'nullable|string|max:255',
             'passportIssueDate' => 'nullable|date_format:d-m-Y',
             'passportExpiryDate' => 'nullable|date_format:d-m-Y',
-            'addressLine1' => 'nullable|string|max:255',
-            'addressLine2' => 'nullable|string|max:255',
-            'townCity' => 'nullable|string|max:255',
-            'state' => 'nullable|string|max:255',
-            'postCode' => 'nullable|string|max:20',
-            'country' => 'nullable|string|max:255',
             'disabilities' => 'nullable|string|max:255',
             'ethnicity' => 'nullable|string|max:255',
             'genderidentity' => 'nullable|string|max:255',
