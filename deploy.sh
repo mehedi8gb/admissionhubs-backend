6a77357aecdf20d9687b1c515b8a229b3f5730c7
mehidy <mehidy.gb@gmail.com>
2025-01-10 02:56:16 +0600

    update path url

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 # Variables
-APP_DIR="/var/www/apex365"
+APP_DIR="/var/www/admissionhubs-backend"
 LOG_FILE="$APP_DIR/deploy.log"
 
 # Start logging
